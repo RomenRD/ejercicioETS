0035aed44221816a2eac9f7c38353d418688ee6f
RomenRD <romenrmsdz@gmail.com>
2022-03-16 15:11:53 +0000

    Clases del proyecto

---
+++ area/src/main/java/es/iespuerto/area/RomenRD.java
@@ -0,0 +1,41 @@
+package es.iespuerto.area;
+
+/**
+ * Hello world!
+ *
+ */
+public class Romenrd{
+    private String tipo;
+    private int valor1;
+    private int valor2;
+    /**
+     * Constructor vacio
+     */
+    public void Romenrd(){
+
+    }
+    /**
+     * Constructor que recibe un parametro
+     * @param valor1
+     */
+    public void Romenrd(int valor1){
+
+    }
+    /**
+     * Constructor que recibe dos parametros
+     * @param valor1
+     * @param valor2
+     */
+    public void Romenrd(int valor1, int valor2){
+        
+    }
+    /**
+     * Metodo para calcular el area
+     * @param valor1
+     * @param valor2
+     * @return valor1*valor2
+     */
+    public int CalculoArea(int valor1, int valor2){
+
+    }
+}
